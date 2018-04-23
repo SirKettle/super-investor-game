e5895feaa1dd360fd8f30178d9d79563c588eaae
Will Thirkettle <william.thirkettle@ubs.com>
2018-04-23 12:00:56 +0100

    preload assets

--- src/states/preloader.ts
+++ src/states/preloader.ts
@@ -1,5 +1,35 @@
+export const assets = {
+  images: {
+    // bg: 'assets/images/blood_bg.jpg',
+  },
+  spritesheets: {
+    //   gamepad: {
+    //     src: 'assets/spritesheets/gamepad.png',
+    //     width: 50,
+    //     height: 50
+    //   }
+  },
+  audio: {
+    // TODO: Add audio
+    // myAudio: 'assets/audio/my-audio.wav'
+  }
+};
+
 export default class Preloader extends Phaser.State {
-  public preload(): void {}
+  public preload(): void {
+    Object.keys(assets.images).forEach(key => {
+      this.game.load.image(key, assets.images[key]);
+    });
+
+    Object.keys(assets.spritesheets).forEach(key => {
+      const { src, width, height } = assets.spritesheets[key];
+      this.game.load.spritesheet(key, src, width, height);
+    });
+
+    Object.keys(assets.audio).forEach(key => {
+      this.game.load.audio(key, assets.audio[key]);
+    });
+  }
 
   public create(): void {
     this.game.camera.onFadeComplete.addOnce(this.loadTitle, this);
