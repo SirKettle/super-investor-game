bd8de229ea8b5fd86c35eb7da575ced393efabf4
Will Thirkettle <hello@willthirkettle.co.uk>
2018-05-01 17:52:02 +0100

    Add taxman pixel file and alter numbers

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -10,12 +10,12 @@ export const toScore = (amount: number): string => {
 
 export const toMoneyFormat = (amount: number): string => {
   const modAmount = Math.round(amount * 1000);
-  if (modAmount > 1000000) {
+  if (modAmount > 999999) {
     return `${CURRENCY_SYMBOL}${(
       Math.floor(modAmount / 100000) / 10
     ).toLocaleString()}M`;
   }
-  if (modAmount > 1000) {
+  if (modAmount > 99999) {
     return `${CURRENCY_SYMBOL}${(
       Math.floor(modAmount / 100) / 10
     ).toLocaleString()}K`;
